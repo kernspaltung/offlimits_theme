33c889c05bdcbfaca5e7b8b375707384b0a74cc6
josecaos <jazzvoon@gmail.com>
2016-05-30 15:27:54 -0500

    destacados sticky listos

--- secciones/avisos_destacados_desktop_sticky.php
+++ secciones/avisos_destacados_desktop_sticky.php
@@ -1,12 +1,114 @@
-<div id="avisos-destacados-sticky-large" class="large-3 columns show-for-large h_20vh" data-sticky-container>
+<div id="avisos-destacados-sticky-large" class="large-3 columns show-for-large p0 h_100vh right" data-sticky-container>
 
-  <div id="avisos-destacados-sticky" class="large-12 columns sticky h_100" data-sticky>
+  <div id="avisos-destacados-sticky" class="large-12 columns sticky pl0 h_100" data-sticky data-margin-top="0">
 
-    avisos destacados sticky
+    <!-- noticia destacada principal -->
+    <a href="#">
+      <div id="avisos-destacados-header" class="large-9 columns p2 m0 h_10vh fontRM text-left">
+        <div class="vcenter">
+          Avisos Destacados
+        </div>
+      </div>
+      <div id="avisos-destacados-header" class="large-3 columns p2 m0 h_10vh fontRL bold text-center">
+        <div class="vcenter">
+          <i class="fa fa-angle-right"></i>
+        </div>
+      </div>
+    </a> <!-- temrina header destacados sticky -->
+
+    <a href="#">
+      <div id="avisos-destacados-principal" class="large-12 columns p0 m0 h_50vh">
+
+        <div id="avisos-destacados-principal-header" class="large-8 columns p2 m0 ha fontRS">
+          <!-- <div class="vcenter"> -->
+          Título aviso principal (últ pub.)
+          Montes ac anomian futas.
+          <!-- </div> -->
+        </div>
+        <div id="avisos-destacados-principal-header-fecha" class="large-4 columns text-center pt1 fontRXS"> 1 January 1970</div>
+
+        <div class="small-12 columns p0 m0 h_50">
+
+          <div id="avisos-destacados-thumb" class="columns h_100 imgLiquid imgLiquidFill cursor-pointer">
+            <img class="" src="http://fakeimg.pl/300" alt="" />
+          </div>
+
+        </div>
+
+        <div class="large-10 columns p2 m0 h_10vh fontRXS">
+          <!-- <div class="vcenter"> -->
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A quaerat, sunt dignissimos.
+          <!-- </div> -->
+        </div>
+
+        <div class="large-2 columns p2 m0 minH_10vh fontRM bold text-center">
+          <div class="vcenter">
+            <i class="fa fa-angle-right"></i>
+          </div>
+        </div>
+
+      </div>
+    </a>
+
+    <!-- avisos secundarios -->
+    <a href="#">
+      <div id="avisos-destacados-secundario" class="large-6 columns p0 m0 h_50vh">
+
+
+        <div class="small-12 columns p0 m0 h_50">
+          <div id="avisos-destacados-thumb" class="columns h_100 imgLiquid imgLiquidFill cursor-pointer">
+            <img class="" src="http://fakeimg.pl/150" alt="" />
+          </div>
+        </div>
+
+        <div id="avisos-destacados-principal-header" class="small-12 columns p2 m0 ha fontRS">
+          <!-- <div class="vcenter"> -->
+          Título aviso anterior 1
+          Montes ac anomian.
+          <!-- </div> -->
+          <div id="avisos-destacados-principal-header-fecha" class="large-12 columns p1 pl2 fontRXXS"> 1 January 1970</div>
+
+        </div>
+
+        <div class="small-12 columns p2 m0 ha fontRXS">
+          <!-- <div class="vcenter"> -->
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A quaerat, sunt dignissimos.
+          <!-- </div> -->
+        </div>
+
+      </div>
+    </a> <!-- termina primer aviso  -->
+
+    <a href="#">
+      <div id="avisos-destacados-secundario" class="large-6 columns p0 m0 h_50vh">
+
+
+        <div class="small-12 columns p0 m0 h_50">
+          <div id="avisos-destacados-thumb" class="columns h_100 imgLiquid imgLiquidFill cursor-pointer">
+            <img class="" src="http://fakeimg.pl/150" alt="" />
+          </div>
+        </div>
+
+        <div id="avisos-destacados-principal-header" class="small-12 columns p2 m0 ha fontRS">
+          <!-- <div class="vcenter"> -->
+          Título aviso anterior 2
+          Montes ac anomian.
+          <!-- </div> -->
+          <div id="avisos-destacados-principal-header-fecha" class="large-12 columns p1 pl2 fontRXXS"> 1 January 1970</div>
+
+        </div>
+
+        <div class="small-12 columns p2 m0 ha fontRXS">
+          <!-- <div class="vcenter"> -->
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A quaerat, sunt dignissimos.
+          <!-- </div> -->
+        </div>
+
+      </div>
+    </a><!-- termina segundo aviso -->
 
 
 
+  </div><!-- Avisos sticky -->
 
-
-  </div>
 </div>
--- secciones/lineup.php
+++ secciones/lineup.php
@@ -1,7 +1,23 @@
-<div id="lineup" class="small-12 columns minH_100vh">
+<div id="lineup" class="small-12 columns p0 minH_100vh">
 
+  <div id="lineup-header" class="small-12 columns p0 h_10vh">
 
-lineup bandas aqui
+    <a href="">
 
+      <div class="small-9 columns h_100">
+        <div class="vcenter">
+          Lineup / Bandas
+        </div>
+      </div>
+
+      <div class="small-3 columns p2 m0 h_100 fontRM text-center">
+        <div class="vcenter">
+          <i class="fa fa-angle-right"></i>
+        </div>
+      </div>
+
+    </a>
+
+  </div>
 
 </div>
