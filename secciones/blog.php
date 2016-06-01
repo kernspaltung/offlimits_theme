9fdb67932b259f2e93981fa95b146f5cd81b4cc7
josecaos <jazzvoon@gmail.com>
2016-06-01 12:42:39 -0500

    seccion home/blog listo

--- secciones/blog.php
+++ secciones/blog.php
@@ -1,6 +1,6 @@
 <div id="blog" class="small-12 columns p0 h_100">
 
-<!-- header secciones home -->
+  <!-- header secciones home -->
   <div id="blog-header" class="small-12 columns p0 h_10vh">
 
     <a href="">
@@ -20,9 +20,134 @@
     </a>
 
   </div>
+  <!-- temina header -->
+
+<!-- blog ultima -->
+  <a href="#">
+    <div id="blog-principal" class="large-12 columns p0 m0 minH_50vh">
+
+      <div id="blog-principal-header" class="small-12 p2 m0 rel">
+        <div id="blog-principal-titulo" class="small-8 columns p0 m0 minH_10vh fontRS">
+          <div class="vcenter">
+          Título aviso principal (últ pub.)
+          Montes ac anomian futas.
+          </div>
+        </div>
+
+        <div id="blog-principal-header-fecha" class="small-4 columns text-center p0 m0 minH_10vh fontRXS">
+          <div class="vcenter">
+            1 January 1970
+          </div>
+        </div>
+      </div>
+
+
+      <div class="small-12 columns p0 m0 minH_50vh">
+
+        <div id="blog-thumb" class="columns h_50vh imgLiquid imgLiquidFill cursor-pointer">
+          <img class="" src="http://fakeimg.pl/300" alt="" />
+        </div>
+
+      </div>
+
+      <div class="small-9 columns p0 m0 h_10vh fontRXS">
+        <div class="vcenter">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A quaerat, sunt dignissimos.
+        </div>
+      </div>
+
+      <div class="small-3 columns p2 m0 minH_10vh fontRM bold text-center">
+        <div class="vcenter">
+          <i class="fa fa-plus"></i>
+        </div>
+      </div>
+
+    </div>
+  </a><!-- termina blog ultima -->
 
 
 
+  <!-- blog anteriores 1-->
+  <a href="#">
+    <div id="blog-secundario" class="large-6 columns p0 m0 minH_10vh">
+
+
+      <div class="small-12 columns p0 m0 minH_20vh">
+        <div id="blog-thumb" class="columns h_30vh imgLiquid imgLiquidFill cursor-pointer">
+          <img class="" src="http://fakeimg.pl/150" alt="" />
+        </div>
+      </div>
+
+      <div id="blog-secundario-header" class="small-9 columns p0 m0 minH_10vh fontRS">
+        <div class="vcenter">
+        Título aviso anterior 1
+        Montes ac anomian.
+        </div>
+      </div>
+      <div id="blog-secundario-header-fecha" class="small-3 columns p2 pl2 minH_10vh fontRXS">
+        <div class="vcenter">
+          1 January 1970
+        </div>
+      </div>
+
+
+      <div class="small-9 columns p0 m0 ha fontRXS">
+        <div class="vcenter">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A quaerat, sunt dignissimos.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A quaerat, sunt dignissimos.
+        </div>
+      </div>
+
+      <div class="small-3 columns p0 m0 minH_10vh fontRM bold text-center">
+        <div class="vcenter">
+          <i class="fa fa-plus"></i>
+        </div>
+      </div>
+
+
+    </div>
+  </a> <!-- termina blog secundario 1  -->
+
+  <!-- blog anteriores 2-->
+  <a href="#">
+    <div id="blog-secundario" class="large-6 columns p0 m0 minH_10vh">
+
+
+      <div class="small-12 columns p0 m0 minH_20vh">
+        <div id="blog-thumb" class="columns h_30vh imgLiquid imgLiquidFill cursor-pointer">
+          <img class="" src="http://fakeimg.pl/150" alt="" />
+        </div>
+      </div>
+
+      <div id="blog-secundario-header" class="small-9 columns p0 m0 minH_10vh fontRS">
+        <div class="vcenter">
+        Título aviso anterior 1
+        Montes ac anomian.
+        </div>
+      </div>
+      <div id="blog-secundario-header-fecha" class="small-3 columns p0 pl2 minH_10vh fontRXS">
+        <div class="vcenter">
+          1 January 1970
+        </div>
+      </div>
+
+
+      <div class="small-9 columns p0 m0 ha fontRXS">
+        <div class="vcenter">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A quaerat, sunt dignissimos.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A quaerat, sunt dignissimos.
+        </div>
+      </div>
+
+      <div class="small-3 columns p0 m0 minH_10vh fontRM bold text-center">
+        <div class="vcenter">
+          <i class="fa fa-plus"></i>
+        </div>
+      </div>
+
+
+    </div>
+  </a> <!-- termina blog secundario 2  -->
 
 
 
