1971c6ac8a704a147999d43c5c5bda34af9d9866
josecaos <jazzvoon@gmail.com>
2016-06-09 14:21:55 -0500

    wf seccion FAQs lista

---
+++ offlimits_faqs.php
@@ -0,0 +1,30 @@
+<?php
+/*Template Name: FAQs*/
+
+get_header();
+?>
+
+
+<!-- template festival -->
+<div id="template-festival" class="small-12 large-9 columns p0">
+
+<?php
+
+get_template_part('secciones/faqs/pregunta');
+
+
+    ## seccion registro
+
+    get_template_part('secciones/compartidas/footer_registro');
+
+    ?>
+
+  </div>
+  <!-- template festival -->
+
+  <?php
+
+  get_template_part('secciones/compartidas/avisos_destacados_desktop_sticky');
+
+
+  get_footer();
---
+++ secciones/faqs/pregunta.php
@@ -0,0 +1,41 @@
+<div id="titulo-seccion-faqs" class="small-12 columns h30vh fontRXXXL text-center">
+  <div class="small-12 columns p1">Off Limits FAQs</div>
+  <div class="small-12 columns fontRL p2">Preguntas frecuentes</div>
+
+</div>
+
+<div class="small-12 columns minH_50vh">
+
+  <?php
+
+  $html = '';
+
+  for ($i=1; $i < 7; $i++) {
+
+    $html .=
+    '<article id="faq" class="small-6 large-12 columns p3 minH_10vh">
+
+      <div id="faq-titulo" class="vcenter fontRL bold p2">'
+
+        . $i . ' ¿Lorem ipsum dolor sit amet, consectetur adipisicing elit. ?
+
+        <div class="small-10 small-offset-1 fontRL p2">
+          lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+
+        </div>
+
+      </div>
+    </article>';
+
+
+  };
+
+  echo $html;
+
+  ?>
+
+
+
+</div>
