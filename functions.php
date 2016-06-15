c038bd63b37d1102868ba6c0976dcd4249947c75
josecaos <jazzvoon@gmail.com>
2016-06-14 21:35:06 -0500

    fixes informacion + test woocommerce custom pages

--- functions.php
+++ functions.php
@@ -88,3 +88,25 @@ function obtener_link($ID) {
 
    return $url;
 }
+
+
+
+// woocommerce supoprt
+// remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+// remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+//
+//
+// add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+// add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+//
+// function my_theme_wrapper_start() {
+//   echo '<section id="main">';
+// }
+//
+// function my_theme_wrapper_end() {
+//   echo '</section>';
+}
+
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+add_theme_support( 'woocommerce' ); }
