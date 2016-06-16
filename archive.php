35f8cebd9123a9e0a108faffb11e61480bb8c7e2
furenku <furenku@gmail.com>
2016-06-16 05:00:02 -0500

    mejoras plantilla blog integrando

--- archive.php
+++ archive.php
@@ -17,7 +17,7 @@ get_header();
          $q -> the_post();
          ?>
 
-         <article class="small-12 medium-6 large-4 columns p0 h_55vh h_sm_80vh p4">
+         <article class="small-12 medium-6 columns p0 h_55vh h_sm_80vh p4">
             <a href="<?php echo get_the_permalink(get_the_ID()); ?>" class="white acento_hover">
 
                <div id="archive-offlimits-thumb" class="expanded row imgLiquidFill imgLiquid h_20vh w_100 mb2" >
@@ -53,6 +53,56 @@ get_header();
       endwhile;
    endif;
 
+
+
+
+   // prefiesta
+
+   ?>
+   <h1>Prefiesta</h1>
+   <?php
+
+
+   $args = array( 'category__in' => $prefiesta -> term_id, 'number_of_posts' => -1 );
+   $q = new WP_Query( $args );
+
+   if( $q -> have_posts() ) :
+      while( $q -> have_posts() ) :
+         $q -> the_post();
+         ?>
+
+         <article class="small-12 medium-6 large-4 columns p0 h_55vh h_sm_80vh p4">
+            <a href="<?php echo get_the_permalink(get_the_ID()); ?>" class="white acento_hover">
+
+               <div id="archive-offlimits-thumb" class="expanded row imgLiquidFill imgLiquid h_20vh w_100 mb2" >
+                  <?php echo get_the_post_thumbnail(); ?>
+               </div>
+
+               <div id="archive-offlimits-titulo-fecha" class="small-12 columns p0 mb1">
+
+                  <div class="small-9 columns p0 text-left fontL">
+                     <?php echo apply_filters( 'the_title', get_the_title()); ?>
+                  </div>
+
+                  <div class="small-3 columns p4 pt0  text-right fontRM">
+                     <?php echo get_the_date('d\.m\.Y', $avisos[0]->ID ); ?>
+                  </div>
+
+               </div>
+
+               <div class="small-12 columns p0">
+                  <div id="archive-offlimits-contenido" class="small-3 columns fontRM text-right p0 ">
+                     <span class="pr1">Ver más</span><i class="fa fa-plus"></i>
+                  </div>
+               </div>
+
+            </a>
+         </article>
+
+         <?php
+
+      endwhile;
+   endif;
    ?>
 
 </section>
