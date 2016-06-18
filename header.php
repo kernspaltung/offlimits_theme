b5812b70939e1915b9e09335a5e2ca9ec8d4528b
furenku <furenku@gmail.com>
2016-06-17 19:29:24 -0500

    migrando

--- header.php
+++ header.php
@@ -14,7 +14,84 @@
    <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
 
 
-   <?php wp_head(); ?>
+
+
+         <?php
+
+         $name = get_bloginfo('name');
+
+         $twitter_user = '@offlimitsmx';
+
+         if( is_single() ) {
+            $name = $name . ': ' . get_the_title();
+            $description = apply_filters( 'the_excerpt', get_the_excerpt() );
+            $url = get_the_permalink( get_the_ID() );
+
+         } else {
+
+            $description = get_bloginfo('description');
+            $url = get_bloginfo('url');
+
+         }
+
+         $thumb_id = get_post_thumbnail_id();
+         $thumb_url = wp_get_attachment_image_src($thumb_id,'large', true);
+         $image = $thumb_url[0];
+
+
+         $date = '';
+         $time = '';
+
+         ?>
+
+         <title><?php echo $name; ?></title>
+
+
+         <?php include_once 'favicons.php'; ?>
+
+
+         <meta name="description" content="<?php echo $description; ?>" />
+
+         <!-- Schema.org markup for Google+ -->
+         <meta itemprop="name" content="<?php echo $name; ?>">
+         <meta itemprop="description" content="<?php echo $description; ?>">
+         <meta itemprop="image" content="<?php echo $image; ?>">
+
+         <!-- Twitter Card data -->
+         <meta name="twitter:card" content="<?php echo $image; ?>">
+         <meta name="twitter:site" content="<?php echo $twitter_user; ?>">
+         <meta name="twitter:title" content="<?php echo $name; ?>">
+         <meta name="twitter:description" content="<?php echo $description; ?>">
+         <!-- <meta name="twitter:creator" content="@author_handle"> -->
+         <!-- Twitter summary card with large image must be at least 280x150px -->
+         <meta name="twitter:image:src" content="<?php echo $image; ?>">
+
+         <!-- Open Graph data -->
+         <meta property="og:title" content="<?php echo $name; ?>"/>
+         <meta property="og:type" content="article" />
+         <meta property="og:url" content="<?php echo $url; ?>" />
+         <meta property="og:image" content="<?php echo $image; ?>" />
+         <meta property="og:description" content="<?php echo $description; ?>" />
+         <meta property="og:site_name" content="<?php echo $name; ?>" />
+         <meta property="article:published_time" content="<?php echo $date; ?>" />
+         <meta property="article:modified_time" content="<?php echo $time; ?>" />
+         <!-- <meta property="article:section" content="Artget_numberic ID" /> -->
+
+         <?php
+         wp_head();
+         ?>
+
+         <script>
+           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+           m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+           })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+           ga('create', 'UA-76176145-1', 'auto');
+           ga('send', 'pageview');
+
+         </script>
+
 
 </head>
 
