26e5af113956e3efe97cf825c48f99bffe12c450
furenku <furenku@gmail.com>
2016-07-12 23:42:07 -0500

    formulario integrantes bandas

--- backend/bandas.php
+++ backend/bandas.php
@@ -39,3 +39,134 @@ function registrar_cpt_bandas() {
 
 	register_post_type( 'banda', $args );
 }
+
+
+
+
+
+function annadir_metabox_banda()
+{
+    add_meta_box("banda-metabox", "Información de Banda", "metabox_banda_markup", "banda", "side", "high", null);
+}
+
+add_action("add_meta_boxes", "annadir_metabox_banda");
+
+
+function metabox_banda_markup($object)
+{
+    wp_nonce_field(basename(__FILE__), "banda-metabox-nonce");
+
+    ?>
+        <div>
+
+            <label for="banda-lugar_de_origen">Lugar de Origen</label>
+            <input name="banda-lugar_de_origen" type="text" value="<?php echo get_post_meta($object->ID, "banda-lugar_de_origen", true); ?>">
+
+            <br>
+
+            <label for="banda-genero_musical">Género Musical</label>
+            <input name="banda-genero_musical" type="text" value="<?php echo get_post_meta($object->ID, "banda-genero_musical", true); ?>">
+
+            <br>
+
+            <label for="banda-anno_formacion">Año de formación</label>
+            <input name="banda-anno_formacion" type="number" value="<?php echo get_post_meta($object->ID, "banda-anno_formacion", true); ?>">
+
+            <br>
+
+				<div id="integrantes">
+
+					<label>Integrantes</label>
+					<br>
+				<?php
+
+				$integrantes = get_post_meta($object->ID, "banda-integrantes", true);
+
+				if( is_array( $integrantes ) ) :
+					foreach( $integrantes as $integrante ) :
+						if( $integrante != "" ) :
+							?>
+
+			            <input name="banda-integrantes[]" type="text" value="<?php echo $integrante; ?>">
+
+							<?php
+						endif;
+					endforeach;
+				endif;
+				?>
+
+				<input name="banda-integrantes[]" type="text" value="">
+
+				</div>
+
+				<button id="annadir_integrante" type="button" name="button">
+					Añadir Integrante
+				</button>
+
+				<input class="repeatable hidden" name="banda-integrantes[]" type="text" value="">
+
+				<br>
+
+        </div>
+
+		  <script type="text/javascript">
+		  $=jQuery.noConflict();
+			$('#annadir_integrante').click(function(){
+				$('input.repeatable').clone().removeClass('repeatable hidden').detach().appendTo('#integrantes');
+			})
+		  </script>
+    <?php
+}
+
+
+function save_bandas_metabox($post_id, $post, $update)
+{
+    if (!isset($_POST["banda-metabox-nonce"]) || !wp_verify_nonce($_POST["banda-metabox-nonce"], basename(__FILE__)))
+        return $post_id;
+
+    if(!current_user_can("edit_post", $post_id))
+        return $post_id;
+
+    if(defined("DOING_AUTOSAVE") && DOING_AUTOSAVE)
+        return $post_id;
+
+    $slug = "banda";
+    if($slug != $post->post_type)
+        return $post_id;
+
+	$banda_lugar_de_origen = "";
+	$banda_anno_formacion = "";
+	$banda_genero_musical = "";
+	$banda_integrantes = "";
+
+
+    if(isset($_POST["banda-lugar_de_origen"]))
+    {
+        $banda_lugar_de_origen = $_POST["banda-lugar_de_origen"];
+    }
+    update_post_meta($post_id, "banda-lugar_de_origen", $banda_lugar_de_origen );
+
+    if(isset($_POST["banda-anno_formacion"]))
+    {
+        $banda_anno_formacion = $_POST["banda-anno_formacion"];
+    }
+    update_post_meta($post_id, "banda-anno_formacion", $banda_anno_formacion );
+
+
+    if(isset($_POST["banda-genero_musical"]))
+    {
+        $banda_genero_musical = $_POST["banda-genero_musical"];
+    }
+    update_post_meta($post_id, "banda-genero_musical", $banda_genero_musical );
+
+
+    if(isset($_POST["banda-integrantes"]))
+    {
+        $banda_integrantes = $_POST["banda-integrantes"];
+    }
+    update_post_meta($post_id, "banda-integrantes", $banda_integrantes );
+
+
+}
+
+add_action("save_post", "save_bandas_metabox", 10, 3);
