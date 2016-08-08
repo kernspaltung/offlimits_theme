9bb80d39d1a294855da52626e3b3fddaad0b2f0d
furenku <furenku@gmail.com>
2016-08-08 05:20:09 -0500

    mejoras visuales inicio

--- offlimits_lineup.php
+++ offlimits_lineup.php
@@ -4,7 +4,7 @@
 get_header();
 ?>
 
-<section id="festival-bandas" class="small-12 columns p0 h_80vh m0 p0">
+<section id="festival-bandas" class="small-12 columns p0 h_80vh mt_sm_10vh  p0 scrollH">
 
   <?php get_template_part('secciones/compartidas/mosaico_bandas'); ?>
 
